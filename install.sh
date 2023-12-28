fead75f61752677e7ca21c333db0fd91570b1cc5
Ezekiel Warren <zekewarren@gmail.com>
2023-12-27 16:37:46 -0800

    feat: dirty install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ROOT_DIR=$SCRIPT_DIR
+
+cd $ROOT_DIR
+
+./build.sh
+cp dist/debug/cpp2b ~/.local/bin/cpp2b
