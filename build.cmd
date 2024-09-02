dbe0b5dc356baa0f15166e5b85a4c2233f27b3ba
Ezekiel Warren <ezekiel@seaube.com>
2024-09-02 09:03:04 -0700

    chore: add xxh3 to build.cmd

--- build.cmd
+++ build.cmd
@@ -105,6 +105,20 @@ if %ERRORLEVEL% neq 0 (
     exit %ERRORLEVEL%
 )
 
+echo INFO: compiling xxh3 module...
+pushd %modules_dir%
+cl /nologo ^
+    /std:c++latest /W4 /MDd /EHsc ^
+    /reference "%modules_dir%\std.ifc" ^
+    /reference "%modules_dir%\std.compat.ifc" ^
+    /c /interface /TP "%root_dir%src\xxh3.cppm" > NUL
+popd
+
+if %ERRORLEVEL% neq 0 (
+    echo ERROR: failed to compile xxh3 module
+    exit %ERRORLEVEL%
+)
+
 if not exist %cppfront% (
     pushd .cache\repos\cppfront\source
     echo INFO: compiling cppfront...
@@ -125,6 +139,7 @@ cl /nologo "%root_dir%.cache/cpp2/source/src/main.cpp" ^
     /reference "%modules_dir%\std.ifc" "%modules_dir%\std.obj" ^
     /reference "%modules_dir%\std.compat.ifc" "%modules_dir%\std.compat.obj" ^
     /reference "%modules_dir%\dylib.ifc" "%modules_dir%\dylib.obj" ^
+    /reference "%modules_dir%\xxh3.ifc" "%modules_dir%\xxh3.obj" ^
     /reference "%modules_dir%\cpp2b.ifc" "%modules_dir%\cpp2b.obj" ^
     /std:c++latest /W4 /MDd /EHsc ^
     /DEBUG:FULL /Zi /FC ^
