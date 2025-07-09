b32d1f6ff891db3c22675e6918a6b6613eaf7552
Ezekiel Warren <zekewarren@gmail.com>
2025-07-08 21:23:57 -0700

    fix: check for specific msvc versions

--- build.cmd
+++ build.cmd
@@ -22,7 +22,38 @@ for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -products * -requires Micr
     set vs_install_dir=%%i
 )
 
-call "%vs_install_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64 -no_logo -vcvars_ver=14.42
+if exist "%vs_install_dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
+    set /p Version=<"%vs_install_dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
+    set LatestVCToolsVersion=!Version: =!
+)
+
+:: Get list of installed MSVC versions, highest first
+set "msvc_root=%vs_install_dir%\VC\Tools\MSVC"
+set "ChosenMSVC="
+
+for /f "delims=" %%v in ('dir /b /ad "%msvc_root%" ^| sort /R') do (
+    set "ver=%%v"
+    for /f "tokens=1,2 delims=." %%a in ("!ver!") do (
+        set "major=%%a"
+        set "minor=%%b"
+    )
+    rem Compare major.minor ≤ 14.43
+    if !major! LSS 14 (
+        set "ChosenMSVC=%%v"
+        goto :found
+    ) else if !major! EQU 14 if !minor! LEQ 43 (
+        set "ChosenMSVC=%%v"
+        goto :found
+    )
+)
+
+echo ERROR: MSVC version ≤ 14.43 not found
+exit 1
+
+:found
+
+:: Call vsdevcmd with selected version
+call "%vs_install_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64 -no_logo -vcvars_ver=%ChosenMSVC%
 
 if "%VCToolsInstallDir%"=="" (
     echo ERROR: missing VCToolsInstallDir after running vsdevcmd.bat
--- share/cpp2b/run_with_msvc_env.cmd
+++ share/cpp2b/run_with_msvc_env.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
@@ -6,7 +7,37 @@ for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -products * -requires Micr
     set vs_install_dir=%%i
 )
 
-call "%vs_install_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64 -no_logo -vcvars_ver=14.42
+if exist "%vs_install_dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
+    set /p Version=<"%vs_install_dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
+    set LatestVCToolsVersion=!Version: =!
+)
+
+:: Get list of installed MSVC versions, highest first
+set "msvc_root=%vs_install_dir%\VC\Tools\MSVC"
+set "ChosenMSVC="
+
+for /f "delims=" %%v in ('dir /b /ad "%msvc_root%" ^| sort /R') do (
+    set "ver=%%v"
+    for /f "tokens=1,2 delims=." %%a in ("!ver!") do (
+        set "major=%%a"
+        set "minor=%%b"
+    )
+    rem Compare major.minor ≤ 14.43
+    if !major! LSS 14 (
+        set "ChosenMSVC=%%v"
+        goto :found
+    ) else if !major! EQU 14 if !minor! LEQ 43 (
+        set "ChosenMSVC=%%v"
+        goto :found
+    )
+)
+
+echo ERROR: MSVC version ≤ 14.43 not found
+exit 1
+
+:found
+:: Call vsdevcmd with selected version
+call "%vs_install_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64 -no_logo -vcvars_ver=%ChosenMSVC%
 
 for /f "tokens=1,* delims= " %%a in ("%*") do set FORWARD_ARGS=%%b
 
